54cfc18f463fb33a700be41d4b1fb87297daee67
Shane <psmnhb@umsystem.edu>
2022-03-09 13:26:24 -0600

    Began working on boilerplate code for Scene State syncing

---
+++ Assets/Scripts/Networking/Components/SceneStateComponent.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Normal.Realtime;
+using UnityEngine;
+
+public class SceneStateComponent : RealtimeComponent<SceneStateModel>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
