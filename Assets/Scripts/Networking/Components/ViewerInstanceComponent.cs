ffe41b7526a7fe0ff05dac5723e3a003e8377f62
Shane McKelvey <psmnhb@umsystem.edu>
2022-03-11 13:41:19 -0600

    Began creating models for viewer-host interaction and annotations
    Learned a bit more about realtime collections, it looks like I will have to make a bunch of models of primitives and then store the models in a realtime collection...

---
+++ Assets/Scripts/Networking/Components/ViewerInstanceComponent.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using Normal.Realtime;
+using UnityEngine;
+
+public class ViewerInstanceComponent : RealtimeComponent<ViewerInstanceModel>
+{
+
+}
