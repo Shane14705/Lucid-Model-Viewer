9f7ddb9145b881ca2d1970ed23582c76f08b039b
Shane McKelvey <psmnhb@umsystem.edu>
2022-03-12 16:51:10 -0600

    Created object view component/model to allow for persistent storage
    Also learned more about realtime collections, so it is time to decide whether I instantiate individual Annotation prefabs with annotation components, or do I create one realtime collection and let a manager/object view component control the display and hiding of them?

---
+++ Assets/Scripts/Networking/Components/ObjectViewManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Normal.Realtime;
+using UnityEngine;
+
+public class ObjectViewManager : RealtimeComponent<ObjectViewModel>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
