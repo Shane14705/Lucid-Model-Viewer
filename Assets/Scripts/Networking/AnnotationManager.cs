265a0f50013610a5501205000a748f9f9e55adc6
Shane McKelvey <psmnhb@umsystem.edu>
2022-03-12 18:29:42 -0600

    Actually began architecting and taking notes for annotation networking
    Make sure to look at notebook as well for more info and issues

---
+++ Assets/Scripts/Networking/AnnotationManager.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//This script is included on each instantiated annotation, and is responsible for handling updates and initialization for annotations after their model is added/removed from the ObjectViewManager's RealtimeDictionary
+public class AnnotationManager : MonoBehaviour
+{
+    
+    //Question: can the annotation model have an owner even though it has no RealtimeComponent (to lock editing to only certain clients)?
+    private AnnotationModel _annotationReference;
+
+    private ObjectViewManager _objectManager;
+
+    private uint _dictKey;
+
+    public uint DictKey
+    {
+        get => _dictKey;
+        set => _dictKey = value;
+    }
+    
+    private void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        //TODO: MAKE SURE THAT WE UNSUBSCRIBE FROM ALL EVENTS ON OUR MODEL BEFORE WE TELL THE OBJECTMANAGER TO REMOVE THE MODEL FROM THE DATASTORE
+        //How will I be getting a reference to our model? Should I do it through the DataStore?
+        
+        //TODO: something to the effect of taking our reference to the model, unsubscribing our events from it, and then passing our ID up to the ObjectViewManager to remove us from the DataStore
+        throw new NotImplementedException();
+    }
+}
--- Assets/Scripts/Networking/Components/ObjectViewManager.cs
+++ Assets/Scripts/Networking/Components/ObjectViewManager.cs
@@ -5,8 +5,17 @@ using UnityEngine;
 
 public class ObjectViewManager : RealtimeComponent<ObjectViewModel>
 {
+    //When we Instantiate an annotation prefab, use this variable to set the prefab's script values (ex: DictKey, model reference, etc)
+    private AnnotationManager _newAnnotation;
+    
+    //Do I need to worry about this kind of thing for AnnotationModels that are added/removed to the collection, or is this just the equivalent of what I need to do when the Add or Remove callbacks are made?
+    protected override void OnRealtimeModelReplaced(ObjectViewModel previousModel, ObjectViewModel currentModel)
+    {
+        base.OnRealtimeModelReplaced(previousModel, currentModel);
+    }
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         
     }
